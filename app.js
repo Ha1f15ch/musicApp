7855bb6ec3f45dd52879b04f5e0b11cce92ea0fa
Alexey <alexy.alexy98@mail.ru>
2022-11-16 23:44:16 +0300

    Поправил считывание файлов с диска, шар папки

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'D:/music')));
 
 app.use('/', login);
 app.use('/main', mainPage);
--- controller/trackController.js
+++ controller/trackController.js
@@ -5,81 +5,106 @@ const fs = require('fs');
 const path = require('path');
 const async = require('async');
 const validator = require('express-validator');
-const { ValidationHalt } = require('express-validator/src/base');
-const { validationResult } = require('express-validator');
-var dir = "D:/music";
+const {
+    ValidationHalt
+} = require('express-validator/src/base');
+const {
+    validationResult
+} = require('express-validator');
+var dir = "G:/Musik_app/musik_app/public/music";
 
 //SHOW ALL TRACKS
-exports.showAllTrack_GET = async function(req, res, next) {
-    
-    // fs.readdir(dir, (err, files) => {
-    //     if(err) {
-    //         return err
-    //     } 
-        
-    //     files.forEach(file => {
-    //         //console.log(file)
-    //         console.log(path.resolve(file))
-    //     }),
-    //     res.render('ListMus', {title: 'Все треки', data: files})
-    // })
+exports.showAllTrack_GET = async function (req, res, next) {
 
-    function givMeFiles(dirr, files){
-    
-        files = files || [];
-          var allFiles = fs.readdirSync(dirr);
-          for (var i =0; i<allFiles.length; i++){
-              var name = dirr + '/' + allFiles[i];
-              if (fs.statSync(name).isDirectory()){
-                  givMeFiles (name, files);
+    try {
+
+        var getFiles = function (dir, files_) {
+
+            files_ = files_ || [];
+            var files = fs.readdirSync(dir);
+            for (var i in files) {
+                var name = dir + '/' + files[i];
+                if (fs.statSync(name).isDirectory()) {
+                    getFiles(name, files_)
                 } else {
-                    files.push(name);
+                    // var re = / /g;
+                    var newName = name.replace(/\s/gi, '_')
+                        fs.rename(name, newName, err => {
+                            if(err) throw err
+                            console.log('Успешно переименовано')
+                        })
+                    var files2 = name.split('/public')[1]
+                    files_.push(files2);
                 }
             }
-            console.log(allFiles)
-        return files;
-    };
-    res.render('ListMus', {title: 'Все треки', data: givMeFiles(dir)}, console.log(givMeFiles(dir)))
-    //console.log(givMeFiles(dir))
-    // givMeFiles(dir)
+            return files_;
+        };
+        console.log(getFiles(dir))
+        res.render('ListMus', {
+            title: 'Все треки',
+            data: getFiles(dir),
+        })
+    }
+    //console.log(getFiles(dir))
+    catch (e) {
+        return next(e)
+    }
+
+
 };
 
 //LOAD ALL TRACKS FROM DIR
-exports.loadMusFromDir_GET = function(req, res, next) {
-    res.send({message: 'Пока что не реализовано LOAD ALL TRACKS FROM DIR'})
+exports.loadMusFromDir_GET = function (req, res, next) {
+    res.send({
+        message: 'Пока что не реализовано LOAD ALL TRACKS FROM DIR'
+    })
 };
 
 //Detail info abou mus :ID
 exports.detailInfoMus_GET = (req, res, next) => {
-    res.send({message: `Пока что не реализовано Detail info about music :ID - ${req.body.id}`})
+    res.send({
+        message: `Пока что не реализовано Detail info about music :ID - ${req.body.id}`
+    })
 };
 
 //Load NEW TRACK GET
-exports.AddNewTrack_GET = function(req, res, next) {
-    res.send({message: `Пока что не реализовано - Load NEW TRACK GET`});
+exports.AddNewTrack_GET = function (req, res, next) {
+    res.send({
+        message: `Пока что не реализовано - Load NEW TRACK GET`
+    });
 };
 
 //Load NEW TRACK POST
 exports.AddNewTrack_POST = (req, res, next) => {
-    res.send({message: `Пока что не реализовано - Load NEW TRACK POST`})
+    res.send({
+        message: `Пока что не реализовано - Load NEW TRACK POST`
+    })
 };
 
 //Edit track GET ID
 exports.EditMus_GET = (req, res, next) => {
-    res.send({message: `Пока что не реализовано - Edit track GET ID`})
+    res.send({
+        message: `Пока что не реализовано - Edit track GET ID`
+    })
 };
 
 //Edit track POST ID
 exports.EditMus_POST = (req, res, next) => {
-    res.send({message: `Пока что не реализовано - Edit track POST ID`})
+    res.send({
+        message: `Пока что не реализовано - Edit track POST ID`
+    })
 };
 
 //Delete mus GET ID
 exports.DeleteMus_GET = (req, res, next) => {
-    res.send({message: `Пока что не реализовано - Delete mus GET ID`})
+    res.send({
+        message: `Пока что не реализовано - Delete mus GET ID`
+    })
 };
 
 //Delete mus POST ID
 exports.DeleteMus_POST = (req, res, next) => {
-    res.send({message: `Пока что не реализовано - Delete mus POST ID`})
+    res.send({
+        message: `Пока что не реализовано - Delete mus POST ID`
+    })
 };
